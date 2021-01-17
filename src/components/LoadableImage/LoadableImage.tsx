60116e46b7ad4d9fa84d6e5654b8c864d2a4213f
Andrey Karcheuskiy <karway211@gmail.com>
2021-01-17 19:26:21 +0300

    feat: smooth image rendering

--- src/components/LoadableImage/LoadableImage.tsx
+++ src/components/LoadableImage/LoadableImage.tsx
@@ -1,19 +1,32 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styles from './LoadableImage.module.css';
+import cn from 'classnames';
 
 type LoadableImageType = {
-  src: string,
-  alt?: string,
+  src: string
+  alt?: string
   onLoad?: () => void
 }
 
 const LoadableImage: React.FC<LoadableImageType> = ({ src, alt = '', onLoad = () => { } }) => {
   const [isLoaded, setIsLoaded] = useState(false);
-  const containerLoaded = isLoaded && styles.containerLoaded;
-  const imageLoaded = isLoaded && styles.imageLoaded;
+  const imageRef = useRef<HTMLImageElement | null>(null);
+
+  useEffect(() => {
+    if (imageRef.current) {
+      imageRef.current.onload = () => setIsLoaded(true);
+      imageRef.current.src = src;
+    }
+  }, [src]);
+
+  console.log(isLoaded);
   return (
-    <div className={`${styles.container} ${containerLoaded}`}>
-      <img className={`${styles.image} ${imageLoaded}`} src={src} alt={alt} />
+    <div className={cn(styles.container, {
+      [styles.containerLoaded]: isLoaded
+    })}>
+      <img src={''} ref={imageRef} className={cn(styles.image, {
+        [styles.imageLoaded]: isLoaded
+      })} alt={alt} />
     </div>
   )
 }
