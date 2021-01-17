e1374da054cede2269eeda1746ded06a05e9fa40
Andrey Karcheuskiy <karway211@gmail.com>
2021-01-17 21:12:09 +0300

    feat: add lazy-loading

--- src/components/LoadableImage/LoadableImage.tsx
+++ src/components/LoadableImage/LoadableImage.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import styles from './LoadableImage.module.css';
 import cn from 'classnames';
+import useOnScreen from '../../hooks/useOnScreen';
 
 type LoadableImageType = {
   src: string
@@ -11,22 +12,29 @@ type LoadableImageType = {
 const LoadableImage: React.FC<LoadableImageType> = ({ src, alt = '', onLoad = () => { } }) => {
   const [isLoaded, setIsLoaded] = useState(false);
   const imageRef = useRef<HTMLImageElement | null>(null);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const isVisible = useOnScreen(containerRef);
 
   useEffect(() => {
+    if (!isVisible || isLoaded) {
+      return;
+    }
     if (imageRef.current) {
-      imageRef.current.onload = () => setIsLoaded(true);
+      imageRef.current.onload = () => {
+        setIsLoaded(true);
+        onLoad();
+      }
       imageRef.current.src = src;
     }
-  }, [src]);
+  }, [src, isVisible, onLoad, isLoaded]);
 
-  console.log(isLoaded);
   return (
-    <div className={cn(styles.container, {
+    <div ref={containerRef} className={cn(styles.container, {
       [styles.containerLoaded]: isLoaded
     })}>
-      <img src={''} ref={imageRef} className={cn(styles.image, {
+      {(isVisible || isLoaded) && (<img src={''} ref={imageRef} className={cn(styles.image, {
         [styles.imageLoaded]: isLoaded
-      })} alt={alt} />
+      })} alt={alt} />)}
     </div>
   )
 }
